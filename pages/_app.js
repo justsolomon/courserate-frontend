b61b9c711b6ab5456d33c5ecc4224037dc18a67f
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-11-10 04:16:17 +0100

    Add component for showing progress when navigating between routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import { ColorModeProvider, CSSReset, ThemeProvider } from '@chakra-ui/core';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <ColorModeProvider>
+        <NextNProgress
+          color='#63B3ED'
+          startPosition={0.1}
+          stopDelayMs={200}
+          height='3'
+        />
         <CSSReset />
         <Component {...pageProps} />
       </ColorModeProvider>
