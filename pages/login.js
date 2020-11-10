97631516fa319336c1f26a196d6dfedbc90877b3
Gbolahan Balogun <gbolahanbalogun5@gmail.com>
2020-11-10 02:09:28 +0100

    Add authentication pages

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import LoginForm from '../components/auth/login/LoginForm';
+import Layout from '../components/global/layout/Layout';
+
+function Login() {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+}
+
+export default Login;
---
+++ pages/signup.js
@@ -0,0 +1,14 @@
+import Layout from '../components/global/layout/Layout';
+import SEO from '../components/global/seo/SEO';
+import SignUpForm from '../components/auth/signup/signUpForm';
+
+function Signup() {
+  return (
+    <Layout>
+      <SEO prefix='Sign Up' />
+      <SignUpForm />
+    </Layout>
+  );
+}
+
+export default Signup;
